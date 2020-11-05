ceca26adceb5b9112332e8173685a2c1929bbdb2
Louise Bollard <louiisebld@gmail.com>
2020-11-05 08:33:18 +0100

    Create view.php

---
+++ view.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Casa Negra</title>
+	<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+	<main>
+		<header>
+			<h1> Casa Negra </h1>
+		</header>
+
+
+<ul>
+		<li><a href="index.php?page=accueil"> Accueil </a></li>
+	</ul>
+
+
+		<?php
+
+
+
+		if (!isset($_GET['page'])) {
+		header("location:index.php?page=accueil");
+
+	}
+
+
+?>
+
+
+
+<footer>
+
+	&copy; CasaNegra 2020
+
+</footer>
+
+</body>
+</html>

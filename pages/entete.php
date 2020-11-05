4d4686b0f79360ce6ed7210d3f2216c6ec72ca48
Louise Bollard <louiisebld@gmail.com>
2020-11-05 11:24:24 +0100

    Update entete.php

--- pages/entete.php
+++ pages/entete.php
@@ -1,13 +1,13 @@
-<?php
+<!-- Menu du site -->
 
-// Menu déroulant du site
+<div id="menu">
+<ul id="listemenu">
+<li id="premier">Accueil</li>
+<li>Présentation</li>
+<li>Avis</li>
+<li>Rendez-vous</li>
+<li id="dernier">Contact</li>
+</ul>
+</div>
+ 
 
-
-
-
-
-
-
-
-
-?>

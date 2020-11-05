f50b209d5b125de16b70f9dc39406b6271e94ce4
Cegeeenre <tomthierry73@gmail.com>
2020-11-05 10:36:09 +0100

    Update accueil.php

---
+++ pages/accueil.php
@@ -0,0 +1,5 @@
+<article>
+	<scrpit>
+		var tabimages = ['imagesRealisations/real1.jpg', 'imagesRealisations/real2.jpg', 'imagesRealisations/real3.jpg', 'imagesRealisations/real4.jpg']
+	</scrpit>
+</article>

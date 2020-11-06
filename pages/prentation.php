2ac684c90c1a4135482cb691e7472e4429ac3ba4
Cegeeenre <tomthierry73@gmail.com>
2020-11-06 21:32:18 +0100

    Create prentation.php

---
+++ pages/prentation.php
@@ -0,0 +1,28 @@
+<section>
+	<article>
+		<div class="prenstation">
+			<h2>Qui sommes nous ?</h2>
+			<p>Casa Negra, agitateur, créateur d'intérieur. Nous sommes une jeune entreprise créé courant 2019/2020 par Marie THIERRY et Hervé FERNANDEZ. Malgré la jeunesse de notre entreprise, nous sommes avons de très solide bases dans le domaine, avec un passé chargé en projets et diffèrentes réalisations dans ce dernier.</p>
+
+			<h2>Notre équipe:</h2>
+			<div class="equipe">
+				<div class="membre">
+					<h2>Marie</h2>
+					<p class="italic">Archictecte/Conceptrice d'intérieur</p>
+					<p>Parcours varié pour Marie, vente dans le prêt a porter, assisatante de direction dans ce même domaine, mais finalement, architecte d'interieur. C'est en 2013 que Marie trouve sa voie, et se lance dedans. C'est là qu'elle commence à travailler pour une entreprise, et qu'elle se fait petit à petit un nom. Porter vers le moderne, c'est dans cette univers qu'elle va se se consacrer le plus souvent. Creative, motivé, et à l'écoute, elle saura donner vie à votre projet.</p>
+				</div>
+				<div class="membre">
+					<h2>Hervé</h2>
+					<p class="italic">Builder</p>
+					<p>Per hoc minui studium suum existimans Paulus, ut erat in conplicandis negotiis artifex dirus, unde ei Catenae inditum est cognomentum, vicarium ipsum eos quibus praeerat adhuc defensantem ad sortem periculorum communium traxit. et instabat ut eum quoque cum tribunis et aliis pluribus ad comitatum imperatoris vinctum perducere</p>
+				</div>
+			</div>
+
+		</div>
+	</article>
+
+
+
+
+
+</section>

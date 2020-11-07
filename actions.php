e6ef38a350c7faec6e9b3c8b0f2fe25c21a618c6
Cegeeenre <tomthierry73@gmail.com>
2020-11-07 11:16:52 +0100

    Update actions.php

--- actions.php
+++ actions.php
@@ -2,4 +2,16 @@
 //Recupertation du tableau de l'equipe
 $equipe = charge_equipe($c);
 
+//initialise la page ou la défini en fonction du choix de l'utilisateur
+if (isset($_GET) && isset($_GET["page"])){
+	$page=$_GET["page"];
+	//On modifie le titre de la page en meme temps que le changement de page
+	$title=$_GET["page"];
+
+}else{
+	//Page de base, sans argument dans l'url ou autre chose que page
+	$page="accueil";
+	$title="accueil";
+}
+
 ?>

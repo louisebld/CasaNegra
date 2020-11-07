67195e86e05035643b144175c25e32265cfec58d
Louise Bollard <louiisebld@gmail.com>
2020-11-07 09:08:05 +0100

    Update prentation.php

--- pages/prentation.php
+++ pages/prentation.php
@@ -1,6 +1,6 @@
 <section>
 	<article>
-		<div class="prenstation">
+		<div class="presentation">
 			<h2>Qui sommes nous ?</h2>
 			<p>Casa Negra, agitateur, créateur d'intérieur. Nous sommes une jeune entreprise créé courant 2019/2020 par Marie THIERRY et Hervé FERNANDEZ. Malgré la jeunesse de notre entreprise, nous sommes avons de très solide bases dans le domaine, avec un passé chargé en projets et diffèrentes réalisations dans ce dernier.</p>
 
@@ -16,6 +16,13 @@
 					<p class="italic">Builder</p>
 					<p>Per hoc minui studium suum existimans Paulus, ut erat in conplicandis negotiis artifex dirus, unde ei Catenae inditum est cognomentum, vicarium ipsum eos quibus praeerat adhuc defensantem ad sortem periculorum communium traxit. et instabat ut eum quoque cum tribunis et aliis pluribus ad comitatum imperatoris vinctum perducere</p>
 				</div>
+			</div>
+
+			<h2> Notre méthode de travail </h2>
+			<div class="methode">
+
+				Casa Negra est une entreprise basée sur la relation entre le client et l'architecte. Nous faisons tout en oeuvre à partir des besoins recherchés, d'établir des croquis, des plans, des images de synthèses pouvant correspondre au mieux à notre client. Chaque membre de notre équipe travaille en cohésion pour vous garantir une avancée fluctuante de votre projet. Chez nous la communication est un point essentiel pour toujours s'assurer que vos idées seront intégrés au projet. Dès lors de nos premiers échanges, nous ferons un devis de votre projet et nous vous proposerons plusieurs solutions répondant à vos besoins. ....
+
 			</div>
 
 		</div>

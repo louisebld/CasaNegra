34c05ebf57e7a46e910f2bf1f528d6086843ee41
Louise Bollard <louiisebld@gmail.com>
2020-11-07 09:07:46 +0100

    Update accueil.php

--- pages/accueil.php
+++ pages/accueil.php
@@ -2,7 +2,7 @@
 <section class="prentation">
 <!-- Petite présentation du site -->
 		<h1>Presentation</h1>
-			<p>L'entrprise Casa Negra, architecte d'intérieur est là pour vous, elle vous accompagne à travers vos démarches, pour donner vie à votre projet.</p>
+			<p>L'entreprise Casa Negra, architecte d'intérieur est là pour vous, elle vous accompagne à travers vos démarches, pour donner vie à votre projet.</p>
 
 			<p>En nous choisissant, vous êtes sur de trouver de la modernité, avec un mélange de créativité qui rebondit sur vos idées pour pouvoir créer de larges perspectives pour votre projet.</p>
 

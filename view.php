4ffa18ea428d399d258735065d2a373fb48cfc89
Cegeeenre <tomthierry73@gmail.com>
2020-11-07 11:17:05 +0100

    Update view.php

--- view.php
+++ view.php
@@ -2,25 +2,34 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	<title>Casa Negra</title>
+	<title><?php echo $title ?></title>
 	<link rel="stylesheet" type="text/css" href="style.css" />
 </head>
 <body>
 	<main>
 		<header>
-			<h1> Casa Negra </h1>
+			<h1><?php echo $title ?></h1>
 		</header>
 
 			
 		<ul>
-			<li><a href="index.php?page=accueil"> Accueil </a></li>
+			<li><a href="index.php?page=accueil"> Accueil </a>
+			</li>
+			<!-- Je ne sais pas pourquoi, il veut pas prendre presentation en lien .. -->
+			<li><a href="index.php?page=presentation"></a>Presentation</li>
 		</ul>
 	</main>
 
 	<?php
+		//Inclusion de la page selon la valeur de $page
+		if ($page == "accueil"){
+			include ("pages/accueil.php");
+		}
 
+		elseif ($page == "presentation"){
+			include ("pages/presentation.php");
+		}
 
-		include 'pages/accueil.php'
 
 
 	?>

d62d1f6968ec2374a5b3b8f1822777cde0fa3377
Juliette-Neyrat <72942347+Juliette-Neyrat@users.noreply.github.com>
2020-11-09 21:41:05 +0100

    Create footer.php

---
+++ pages/footer.php
@@ -0,0 +1,12 @@
+<?php
+
+?>
+<footer> 
+<p> Pour nous contacter : </p>
+<p><a href="contact@casanegra.fr">Notre Email</a></p>
+<p><a href="tel:+33626973516"> Marie au 06.26.97.35.16</a> ou <a href="tel:+33623757043> Hervé au 06.23.76.70.43 </a></p>
+
+</footer>
+
+<?php
+?>

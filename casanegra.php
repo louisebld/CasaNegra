ad71b3fd097350747a6f2bb0ba2462a83417b6e1
Louise Bollard <louiisebld@gmail.com>
2020-11-10 13:37:34 +0100

    Update casanegra.php

--- casanegra.php
+++ casanegra.php
@@ -77,6 +77,49 @@ function print_equipe($equipe){
 }
 
 
+function charge_compte($c){
+	//Fonction recupere le tableau de la bdd Equipe de salarie + caractéristique
+
+	//requete
+	$sql="SELECT * FROM compte";
+	$result=  mysqli_query($c, $sql);
+
+	//on met dans un tableau
+	$tableau = [];
+	while ($row=mysqli_fetch_assoc($result)) {
+		$tableau[] = $row;
+	}
+	//var_dump($tableau);
+	return $tableau;
+}
+
+
+
+
+
+function print_formulaire_connexion() {
+	//Affiche le formulaire pour ajouter un nouveau membre
+
+	?>
+
+	<form method="post" action="index.php?page=connexion">
+
+		<p><label for="ajout"> Adresse Mail </label>
+			<input type = "text" name ="mail" id="ajout" value="<?php if (isset($_SESSION['donnee']['mail'])) 
+																echo $_SESSION['donnee']['mail']; ?>"></p>
+
+		<p><label for="ajout"> Mot de passe </label>
+	<input type = "password" name ="motdepasse" id="ajout" value="<?php if (isset($_SESSION['donnee']['motdepasse'])) 
+														echo $_SESSION['donnee']['motdepasse']; ?>"></p>
+
+
+		<p><input type="submit" name="connexion" id="action" value="S'inscrire"/></p>
+				</form>
+
+				<?php	 
+			}
+
+
 
 ?>
 

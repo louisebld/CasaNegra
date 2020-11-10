c3e80e1c7cdfb22deb4212f83acb08022ec6b00e
Cegeeenre <tomthierry73@gmail.com>
2020-11-10 13:11:24 +0100

    Update footer.php

--- pages/footer.php
+++ pages/footer.php
@@ -1,12 +1,8 @@
-<?php
 
-?>
 <footer> 
 <p> Pour nous contacter : </p>
 <p><a href="contact@casanegra.fr">Notre Email</a></p>
-<p><a href="tel:+33626973516"> Marie au 06.26.97.35.16</a> ou <a href="tel:+33623757043> Hervé au 06.23.76.70.43 </a></p>
+<p><a href="tel:+33626973516"> Marie au 06.26.97.35.16</a> ou <a href="tel:+33623757043"> Hervé au 06.23.76.70.43 </a></p>
 
 </footer>
 
-<?php
-?>

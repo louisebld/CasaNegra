e6f6141daf7766b1fe7c5682d804fc0c8d9428aa
Cegeeenre <tomthierry73@gmail.com>
2020-11-10 15:16:02 +0100

    Update funct_equipe.php

--- functions/funct_equipe.php
+++ functions/funct_equipe.php
@@ -10,21 +10,23 @@ table equipe:
 	tel: varchar(10)
 */
 
-function charge_equipe($c){
-	//Fonction recupere le tableau de la bdd Equipe de salarie + caractéristique
 
-	//requete
-	$sql="SELECT * FROM equipe";
-	$result=  mysqli_query($c, $sql);
+function print_equipe($equipe){
+	//Print toute l'equipe contenue dans la base de donnée
+	echo '<div class="equipe">';
+	foreach ($equipe as $key => $value) {
+		echo '<div class="membre_equipe">';
+		echo "<h2>" . $value["name"];
+		echo $value["fname"] . "</h2>";
+		echo "<p>" . $value["age"] . ", " . $value["metier"] . "</p>" ;
+		echo "numéro de téléphone: " . $value["tel"];
+		echo "<p>" . $value["description"] . "</p>";
 
-	//on met dans un tableau
-	$tableau = [];
-	while ($row=mysqli_fetch_assoc($result)) {
-		$tableau[] = $row;
+		echo "</div>";
 	}
-	//var_dump($tableau);
-	return $tableau;
+	echo "</div>"; 
 }
+
 
 
 

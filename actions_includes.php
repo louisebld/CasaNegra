5193621c304c04c549ba1e914f1ec94e8bdf7586
Louise Bollard <louiisebld@gmail.com>
2020-11-10 23:01:15 +0100

    Create actions_includes.php

---
+++ actions_includes.php
@@ -0,0 +1,11 @@
+<?php
+
+// Toutes les includes de nos fichiers actions php
+
+
+include 'actions/actions_page.php';
+include 'actions/actions_inscription.php';
+include 'actions/actions_connexion.php';
+
+
+?>

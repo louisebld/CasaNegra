6c590403ae42f3f1571d1206c3a6dccd3976a172
Cegeeenre <tomthierry73@gmail.com>
2020-11-10 14:59:42 +0100

    Update funct_equipe.php

--- functions/funct_equipe.php
+++ functions/funct_equipe.php
@@ -26,21 +26,6 @@ function charge_equipe($c){
 	return $tableau;
 }
 
-function print_equipe($equipe){
-	//Print toute l'equipe contenue dans la base de donnée
-	echo '<div class="equipe">';
-	foreach ($equipe as $key => $value) {
-		echo '<div class="membre_equipe">';
-		echo "<h2>" . $value["name"];
-		echo $value["fname"] . "</h2>";
-		echo "<p>" . $value["age"] . ", " . $value["metier"] . "</p>" ;
-		echo "numéro de téléphone: " . $value["tel"];
-		echo "<p>" . $value["description"] . "</p>";
-
-		echo "</div>";
-	}
-	echo "</div>";
-}
 
 
 

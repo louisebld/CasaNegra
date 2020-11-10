b1d5ac6c30edd51b66f038fe986a0886ee744477
Louise Bollard <louiisebld@gmail.com>
2020-11-10 14:54:19 +0100

    Create funct_includes.php

---
+++ funct_includes.php
@@ -0,0 +1,10 @@
+<?php
+
+// Toutes les includes de nos fichiers fonctions php
+
+include 'functions/funct_compte.php';
+include 'functions/funct_equipe.php';
+include 'functions/funct_bddcompte.php';
+include 'functions/funct_bddequipe.php';
+
+?>

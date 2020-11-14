0e50586dd30e8ca6976ca8c78f392467c7187ac5
Cegeeenre <tomthierry73@gmail.com>
2020-11-14 10:38:37 +0100

    Update funct_includes.php

--- funct_includes.php
+++ funct_includes.php
@@ -3,7 +3,9 @@
 // Toutes les includes de nos fichiers fonctions php
 include 'functions/funct_compte.php';
 include 'functions/funct_equipe.php';
+include 'functions/funct_avis.php';
+
 include 'functions/funct_bddcompte.php';
 include 'functions/funct_bddequipe.php';
-
+include 'functions/funct_bddavis.php';
 ?>

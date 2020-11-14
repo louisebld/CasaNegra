24e045ebb11e43455363f1fb0256940fc58366ae
Louise Bollard <louiisebld@gmail.com>
2020-11-14 17:20:35 +0100

    Update funct_bddcompte.php

--- functions/funct_bddcompte.php
+++ functions/funct_bddcompte.php
@@ -26,6 +26,29 @@ function charge_compte_mailmotdepasse($c){
 	return $tableau;
 }
 
+function existeemail ($mail) {
+// retourne si le mail est déjà associé à un compte dans la base de donnée oui ou non
+	global $c;
+	// Je fais une recherche dans la base à partir de l'adresse mail
+	$email = mysqli_query($c, "SELECT * FROM compte WHERE mail = '".$mail."'");
+	$compteur = mysqli_num_rows($email);
+	if ($compteur>0) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
 
+// Fonction pour récupérer l'email d'un compte avec l'adresse mail lors de la connexion
+function recupereid($mail) {
+	global $c;
+// on selectionne le compte que l'on veut à partir de l'email
+	$email = mysqli_query($c, "SELECT * FROM compte WHERE mail = '".$mail."'");
+	$resultat=mysqli_fetch_assoc($email);
+	$id=$resultat['idcompte'];
+	return $id;
+
+}
 
 ?>

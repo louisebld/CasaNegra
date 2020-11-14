1b2205a7a89be56284f03fc594b5976f70027c01
Cegeeenre <tomthierry73@gmail.com>
2020-11-14 10:38:32 +0100

    Update avis.php

--- pages/avis.php
+++ pages/avis.php
@@ -1,6 +1,23 @@
 <section class="avis">
-	<h2>Vous nous faites confiance, voyez les avis des clients:</h2>
+	<h2>Ajouter un avis</h2>
+		<?php
+			var_dump($_SESSION['monadressemail']);
+			var_dump($_SESSION);
+			var_dump($_POST);
+			print_form_avis();
+			if (isset($_SESSION['faute'])) {
+				echo "<ul>";
+				foreach($_SESSION["faute"] as $faute){
+					echo "<li>$faute</li>";
+				}
+				echo "</ul>";
+			}
 
-
+			
+		?>
+	<h2>Vous nous faites confiance, voici les avis des clients:</h2>
+		<?php
+			print_avis($avis)
+		?>
 
 </section>

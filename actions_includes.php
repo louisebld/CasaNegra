92e6c735a66f6bbaf7fa3c71cb6589b21d8cbc78
Cegeeenre <tomthierry73@gmail.com>
2020-11-16 08:11:20 +0100

    Update actions_includes.php

--- actions_includes.php
+++ actions_includes.php
@@ -2,6 +2,7 @@
 
 // Toutes les includes de nos fichiers actions php
 
+include 'actions/actions_avis.php';
 include 'actions/actions_page.php';
 include 'actions/actions_inscription.php';
 include 'actions/actions_connexion.php';

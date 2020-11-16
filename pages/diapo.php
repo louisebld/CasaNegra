9459bf84ed3ecd196a9202b6c5c621184ff2a2b7
Juliette-Neyrat <72942347+Juliette-Neyrat@users.noreply.github.com>
2020-11-16 08:52:35 +0100

    Update diapo.php

--- pages/diapo.php
+++ pages/diapo.php
@@ -1,16 +1,50 @@
 <script>
-i=0;
-function diaporamaSuivant() {
-     var tabImages = new Array("images/diapo1.jpg", "images/diapo2.jpg","images/diapo3.jpg","images/diapo4.jpg","images/diapo5.jpg","images/diapo6.jpg","images/diapo7.jpg","images/diapo8.jpg","images/diapo9.jpg","images/diapo10.jpg","images/diapo11.jpg");
-     document.getElementById("diaporama").src = tabImages[i];
-     i++;
-     setTimeout(diaporamaSuivant,5000); //5000 pour changer d'images toutes les 5 secondes.
+var diaporama = 1;
+affichage(diaporama);
+
+function boutons(n) {
+  affichage(diaporama += n);
 }
+
+
+function affichage(n) {
+  var i;
+  var diapoImg = document.getElementsByClassName("diapo");
+  if (n > diapoImg.length) {diaporama = 1}    
+  if (n < 1) {diaporama = diapoImg.length}
+  for (i = 0; i < diapoImg.length; i++) {
+     
+	 diapoImg[i].style.opacity = "0";
+  }
+ 
+  diapoImg[diaporama-1].style.opacity = "1";  
+ /*setTimeout(boutons(1), 3000);*/
+
+}
+
+
+
 </script>
+
 <html>
-<body onload="diaporamaSuivant();">
-<div id="diapo">
-<img src="images/diapo1.jpg" id="diaporama" />
-     </div>
 <body>
+<div class="cadre-diapo">
+<img class="diapo" src="images/diapo1.jpg" alt>
+<img class="diapo" src="images/diapo2.jpg" alt>
+<img class="diapo" src="images/diapo3.jpg" alt>
+<img class="diapo" src="images/diapo4.jpg" alt>
+<img class="diapo" src="images/diapo5.jpg" alt>
+<img class="diapo" src="images/diapo6.jpg" alt>
+<img class="diapo" src="images/diapo7.jpg" alt>
+<img class="diapo" src="images/diapo8.jpg" alt>
+<img class="diapo" src="images/diapo9.jpg" alt>
+<img class="diapo" src="images/diapo10.jpg" alt>
+<img class="diapo" src="images/diapo11.jpg" alt>
+<button class="precedent" aria-label="précédent" onclick="boutons(-1)">❮</butto>
+<button class="suivant" aria-label="suivant" onclick="boutons(1)">❯</button>
+</div>
+
+</body>
 </html>
+
+

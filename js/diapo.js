2bbb3270af298f6183e548487f5b000fe5c1a5c1
Juliette-Neyrat <72942347+Juliette-Neyrat@users.noreply.github.com>
2020-11-16 10:56:21 +0100

    Create diapo.js

---
+++ js/diapo.js
@@ -0,0 +1,26 @@
+var diapositive = 1;
+var timeout = -1;
+
+function boutons(n) {
+	clearTimeout(timeout);
+  affichage(diapositive += n);
+
+}
+
+
+function affichage(n) {
+  var i;
+  var diapoImg = document.getElementsByClassName("diapo");
+  if (n > diapoImg.length) {diapositive = 1}    
+  if (n < 1) {diapositive = diapoImg.length}
+ /* ------------------- animation ----------------------------*/
+  for (i = 0; i < diapoImg.length; i++) {
+     
+	 diapoImg[i].style.opacity = "0";
+  }
+ 
+  diapoImg[diapositive-1].style.opacity = "1";  
+ timeout = setTimeout(function(){boutons(1);}, 3000);
+
+}
+affichage(diapositive);

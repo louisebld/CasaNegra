bb797ea95c4f47ce09c4423c60dedb50d54be331
Cegeeenre <tomthierry73@gmail.com>
2020-11-16 17:22:33 +0100

    Update funct_includes.php

--- funct_includes.php
+++ funct_includes.php
@@ -6,8 +6,10 @@ include 'functions/funct_equipe.php';
 include 'functions/funct_avis.php';
 include 'functions/funct_admin.php';
 include 'functions/funct_diapo.php';
+include 'functions/funct_projet.php';
 
 
+include 'functions/funct_bddprojet.php';
 include 'functions/funct_bddcompte.php';
 include 'functions/funct_bddequipe.php';
 include 'functions/funct_bddavis.php';

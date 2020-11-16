5f8566e870e4e0235ff554ab2c9fb7b59d44e95f
Louise Bollard <louiisebld@gmail.com>
2020-11-16 14:10:13 +0100

    Update funct_includes.php

--- funct_includes.php
+++ funct_includes.php
@@ -4,8 +4,12 @@
 include 'functions/funct_compte.php';
 include 'functions/funct_equipe.php';
 include 'functions/funct_avis.php';
+include 'functions/funct_admin.php';
+
 
 include 'functions/funct_bddcompte.php';
 include 'functions/funct_bddequipe.php';
 include 'functions/funct_bddavis.php';
+include 'functions/funct_bddadmin.php';
+
 ?>

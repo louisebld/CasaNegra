402047eb36c97de8e5d071d55b5995d34dc43226
Cegeeenre <tomthierry73@gmail.com>
2020-11-16 08:11:28 +0100

    Update avis.php

--- pages/avis.php
+++ pages/avis.php
@@ -2,7 +2,9 @@
 	<h2>Ajouter un avis</h2>
 		<?php
 	//		var_dump($_SESSION['monadressemail']);
+			echo "bonjour";
 			var_dump($_SESSION);
+			echo "bonjour";
 			var_dump($_POST);
 			print_form_avis();
 			if (isset($_SESSION['faute'])) {

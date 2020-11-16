07e0a7d84315b62c7d56f8a1efd65485a0ac9b91
Louise Bollard <louiisebld@gmail.com>
2020-11-16 14:10:33 +0100

    Create pageadmin.php

---
+++ pages/pageadmin.php
@@ -0,0 +1,16 @@
+
+<h2> Liste des membres inscrits </h2>
+
+<div id="affichemembre">
+<?php
+affichetouscomptes($donneescomptes);
+?>
+</div>
+<h2> Ajouter un membre dans l'équipe </h2>
+
+<?php
+
+
+recupmetier ($c);
+?>
+

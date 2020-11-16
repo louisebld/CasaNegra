ad9b4412b867e520b8142fc2308f4bf806e67dd5
Louise Bollard <louiisebld@gmail.com>
2020-11-16 17:11:09 +0100

    Update funct_admin.php

--- functions/funct_admin.php
+++ functions/funct_admin.php
@@ -1,7 +1,6 @@
 <?php
 
 
-
 function print_formulairenouveauequipe() {
 	//Affiche le formulaire pour ajouter un nouveau membre
 
@@ -17,13 +16,15 @@ function print_formulairenouveauequipe() {
 			<p>
 				<input type = "text" placeholder="Age" name ="age" id="ajout" ></p>
 
-			<p>
-				<input type = "text" placeholder="Prénom" name ="prénom" id="ajout" ></p>
+			<?php
+			listederoulmetier ();
+			?>
 
 			<p>
 				<textarea id="com" placeholder="Description" name="description" rows="10" cols="35"></textarea>
 			</p>
 
+			
 
 				<p><input type="submit" name="envoiequipe" id="action" value="Ajouter"/></p>
 			</form>
@@ -32,18 +33,35 @@ function print_formulairenouveauequipe() {
 	}
 
 function listederoulmetier () {
-	global $c;
+	global $listemetier;
 ?>
 	<select name="metier" size="1">
-<option>lundi
-
-</select>
-
 <?php
 
 
+for ($i = 0; $i < count($listemetier); $i++){
+	$metiercourant = $listemetier[$i];
+
+	echo "<option>" . $metiercourant['metier'];
+}
+?>
+</select>
+
+<?php
+}
 
 
+// Formulaire pour ajouter un métier
+
+function printformulairemetier() {
+
+?>
+	<form method="post" action="index.php?page=pageadmin">
+	<p><input type = "text" placeholder="Métier" name ="metier" id="ajout" ></p>
+	<p><input type="submit" name="envoiemetier" id="metier" value="Ajouter"/></p>
+	</form>
+
+<?php
 }
 
 

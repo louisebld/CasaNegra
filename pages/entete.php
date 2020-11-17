1a6c5b1ed8bfdf4f27e268b529b7d872a90102de
Juliette-Neyrat <72942347+Juliette-Neyrat@users.noreply.github.com>
2020-11-17 11:20:46 +0100

    Update entete.php

--- pages/entete.php
+++ pages/entete.php
@@ -5,6 +5,7 @@
 <ul id="listemenu">
 <li id="premier"><a href="index.php?page=accueil"> Accueil </a></li>
 <li id="deuxieme"><a href="index.php?page=presentation"> Présentation </a></li>
+<li><a href="index.php?page=projet"> Projets </a></li>
 <li><a href="index.php?page=avis"> Avis </a></li>
 <li><a href="index.php?page=prendrerdv"> Rendez-vous </a></li> 
 <li id="dernier"><a href="index.php?page=contact"> Contact </a></li>

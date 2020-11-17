dd803152845222a74c1cac711e1430d1b8e0b9b6
Louise Bollard <louiisebld@gmail.com>
2020-11-17 17:52:09 +0100

    Update funct_admin.php

--- functions/funct_admin.php
+++ functions/funct_admin.php
@@ -9,22 +9,29 @@ function print_formulairenouveauequipe() {
 		<form method="post" action="index.php?page=pageadmin">
 
 			<p>
-				<input type = "text" placeholder="Nom" name ="nom" id="ajout" ></p>
+				<input type = "text" placeholder="Nom" name ="nom" id="ajout" value="<?php if (isset($_SESSION['donnee']['nom'])) 
+																echo $_SESSION['donnee']['nom']; ?>" ></p>
 			<p>
-				<input type = "text" placeholder="Prénom" name ="prénom" id="ajout" ></p>
+				<input type = "text" placeholder="Prénom" name ="prénom" id="ajout" value="<?php if (isset($_SESSION['donnee']['prénom'])) 
+																echo $_SESSION['donnee']['prénom']; ?>" ></p>
 
 			<p>
-				<input type = "text" placeholder="Age" name ="age" id="ajout" ></p>
+				<input type = "text" placeholder="Age" name ="age" id="ajout" value="<?php if (isset($_SESSION['donnee']['age'])) 
+																echo $_SESSION['donnee']['age']; ?>" ></p>
 
 			<?php
 			listederoulmetier ();
 			?>
 
 			<p>
-				<textarea id="com" placeholder="Description" name="description" rows="10" cols="35"></textarea>
+				<textarea id="com" placeholder="Description" name="description" rows="10" cols="35" value="<?php if (isset($_SESSION['donnee']['description'])) 
+																echo $_SESSION['donnee']['description']; ?>"></textarea>
 			</p>
-
+<!-- la description reste pas -->
 			
+			<p>
+				<input type = "text" placeholder="Téléphone" name ="tel" id="ajout" value="<?php if (isset($_SESSION['donnee']['tel'])) 
+														echo $_SESSION['donnee']['tel']; ?>" ></p>
 
 				<p><input type="submit" name="envoiequipe" id="action" value="Ajouter"/></p>
 			</form>

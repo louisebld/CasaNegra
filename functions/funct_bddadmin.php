263b74f0a41ebd998b6ff35e616925944c37cb32
Louise Bollard <louiisebld@gmail.com>
2020-11-17 17:52:13 +0100

    Update funct_bddadmin.php

--- functions/funct_bddadmin.php
+++ functions/funct_bddadmin.php
@@ -1,7 +1,7 @@
 <?php
 /*Table admin:
 	//Stock les comptes admins via les id des comptes
-	idcompte: int*/
+idcompte: int*/
 
 
 
@@ -13,7 +13,7 @@ function testif_admin($idcompte){
 // si il y a au moins une ligne qui correspond : true
 	if ($nbcompte>0){
 		return true;
-		}
+	}
 	else {
 		return false;
 	}
@@ -44,3 +44,44 @@ function existemetier ($metier) {
 		return false;
 	}
 }
+
+
+
+
+// insertion dans la base de donnée
+// le membre de l'équipe
+
+function insert_membre($nom, $prénom, $age, $idmetier, $description, $tel) {
+
+	// var_dump($nom);
+	// var_dump($prénom);
+	// var_dump($age);
+	// var_dump($idmetier);
+
+	// var_dump($description);
+	// var_dump($tel);
+	global $c;
+	mysqli_query($c, "INSERT INTO equipe(name, fname, age, id_metier, description, tel) values ('$nom', '$prénom', $age, $idmetier, '$description', '$tel')");
+
+}
+
+
+// Cette fonction sert à savoir si il n'y a pas déjà un membre de l'équipe s'appelant comme ça
+function verifmembreequipe ($nom, $prénom) {
+
+	global $c;
+	// Je fais une recherche dans la base à partir de l'adresse mail
+	$nomprénom = mysqli_query($c, "SELECT * FROM equipe WHERE name = '".$nom."' and fname = '".$prénom."'");
+	$compteur = mysqli_num_rows($nomprénom);
+	if ($compteur>0) {
+		return true;
+	}
+	else {
+		return false;
+	}
+
+
+
+}
+
+

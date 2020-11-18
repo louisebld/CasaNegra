9177f2acfe5eed7cdf360b56fcd4789e4ee399a4
Louise Bollard <louiisebld@gmail.com>
2020-11-18 21:40:55 +0100

    Update funct_bddmetier.php

--- functions/funct_bddmetier.php
+++ functions/funct_bddmetier.php
@@ -5,15 +5,15 @@
 function recuperemetier($met) {
 	global $c;
 
-	var_dump($met);
-	$met='Tapissier';
+	//var_dump($met); // pb pas de message qd architecte d'intérieur
+	//$met='Tapissier';
 	// marche plus ...
 // on selectionne le métier que l'on veut à partir de son nom
 	// Je fais une recherche dans la base à partir du métier
 	// $test = ".$metier.";
 	$metier = mysqli_query($c, "SELECT id FROM metier WHERE metier = '$met'");
 	$resultat = mysqli_fetch_assoc($metier);
-	var_dump($resultat);
+
 	$id=$resultat['id'];
 	return $id;
 

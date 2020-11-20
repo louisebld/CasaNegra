d2ffe36ac0ebc86316c40c97ef94518c84b48382
Cegeeenre <tomthierry73@gmail.com>
2020-11-20 22:02:04 +0100

    Update actions_projet.php

--- actions/actions_projet.php
+++ actions/actions_projet.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 // Envoi du formulaire pour créer un projet
 // Vérification
 
@@ -76,7 +76,62 @@ if (isset($_POST['action'])) {
 
 	}
 }
-
+*/
 // Fin la creation du projet
 
 ?>
+
+
+<?php
+	//Partie refaite
+	$projets = charge_projet($c);
+	$nbphotos = charge_nbphotos($c);
+
+
+   if(isset($_FILES['image'])){
+      $errors= array();
+      $file_name = $_FILES['image']['name'];
+      $file_size =$_FILES['image']['size'];
+      $file_tmp =$_FILES['image']['tmp_name'];
+      $file_type=$_FILES['image']['type'];
+
+  
+      	//if (isset($_POST['date']) && isset($_POST['autor']) && isset($_POST['descrition'])){
+	      	//On transporte le fichier dans le dossier image
+/*			if(file_exists("../images/dispo12.php")){
+				unlink("../images/dispo12.php");
+			}*/
+			//Place it into your "uploads" folder mow using the move_uploaded_file() function
+	        move_uploaded_file($file_tmp,"./projets/".$file_name);
+
+	        
+
+/*	        //On le renome pour pouvoir l'ajouter a notre diapo
+			$CheminFichier = "projets/". $file_name;
+			$CheminCible = "projets/ ". "diapo" . $nbphotos . "." . get_file_extension($file_name);
+				
+			if (file_exists($CheminFichier)) {
+
+				if (!rename($CheminFichier, $CheminCible)) {
+					echo "Probleme pour renommer le répertoire $foldername en $RequeteRefNum<br />\n";
+				}
+				else {
+					echo "Fichier renommé ! <br />\n";
+				}
+				
+			}
+			else {
+				echo "Le fichier $CheminFichier n'a pas été trouvé.<br />\n";
+			}
+			*/
+			$date_creation = $_POST['date'];
+			$autor = $_POST['autor'];
+			$description = $_POST['description'];
+
+
+			insert_projet($autor, $description, $file_name, $date_creation);
+	        echo "Success";
+	        unset($_FILES['image']);
+	        header("Location: index.php?page=projet");
+	    //}
+   }

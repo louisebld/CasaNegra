fa55e34ea6a034b62ae3374b710436322a9c4b4c
Louise Bollard <louiisebld@gmail.com>
2020-11-27 14:53:31 +0100

    Update contact.php

--- pages/contact.php
+++ pages/contact.php
@@ -5,5 +5,15 @@
 
 print_formulaire_contact();
 
+if (isset($_SESSION['fautecontact'])) {
+	echo "<ul>";
+	foreach($_SESSION["fautecontact"] as $faute)
+		echo "<li>$faute</li>";
+	echo "</ul>";
+
+}
+
+
+
 ?>
 </section>

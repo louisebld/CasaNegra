4d6ad5461e4055f2021771d32c045ed0a25f736a
Louise Bollard <louiisebld@gmail.com>
2020-12-01 18:36:30 +0100

    Update projet.php

--- pages/projet.php
+++ pages/projet.php
@@ -4,12 +4,15 @@
 
 <section>
 		<?php
+
+		if (isset($_SESSION['idcompte'])){
 			if (testif_admin($_SESSION['idcompte'])){
 				?>
 					<h2>Ajouter un projet:</h2>
 				<?php
 				print_formulaire();
 			}
+		}
 
 	    ?>  
 	    <h2>Nos creations</h2>

1b11a093b10db88e8731cb02243475929d11248c
Louise Bollard <louiisebld@gmail.com>
2020-12-01 16:24:44 +0100

    Update funct_avis.php

--- functions/funct_avis.php
+++ functions/funct_avis.php
@@ -17,20 +17,25 @@ function print_avis($avis){
 	echo '</div>';
 }
 
-function print_form_avis(){
+function print_form_avis($idprojet){
 	//Fonction qui permet d'afficher le formulaire qui ajoute un avis
 	//Obligation d'etre connecté, sinon redirection
 	?>
 
-	<form method="post" action="index.php?page=avis">
+	<form method="post" action="index.php?page=projets">
 
 		<p>
 			<textarea id="com" placeholder="Votre avis.." name="com"
 			rows="10" cols="35"></textarea>
 		</p>
 
+		<p>
+			<?php	
+				echo  '<input id="idprojet" name="idprojet" type="hidden" value= '. $idprojet . ">" ;
+			?>
+		</p>
 
-		<p><input type="submit" name="avis" id="avis" value="Enregistrer"/></p>
+		<p><input type="submit" name="envoyer_com" id="envoyer_com" value="Enregistrer"/></p>
 	</form>
 
 		<?php	 

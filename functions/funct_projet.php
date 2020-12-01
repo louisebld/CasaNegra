9ded2d2f590a95c515bbea3b54146b36d55cb88d
Louise Bollard <louiisebld@gmail.com>
2020-12-01 16:24:53 +0100

    Update funct_projet.php

--- functions/funct_projet.php
+++ functions/funct_projet.php
@@ -25,12 +25,24 @@
 			echo "Realisation: " . $value['autor'] .'</br>';
 			echo $value['description'];
 
+
+			if ($_SESSION['connected']){
+
+				echo "<h2>Ajouter un commentaire</h2>";
+				print_form_avis($value['id']);
+			
+			}
+
+
+
 			$avis = charge_avis($value['id']);
 			if (!empty($avis)) {
 					echo "<h2>Commentaire: </h2></br>";	
+					print_avis($avis);
 			}
 
-			print_avis($avis);
+
+
 
 			echo "</figcaption></figure>";
 

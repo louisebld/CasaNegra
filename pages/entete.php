459ce31a5adb280636fdbad8f5facecd0aeeb534
Louise Bollard <louiisebld@gmail.com>
2020-12-01 16:25:12 +0100

    Update entete.php

--- pages/entete.php
+++ pages/entete.php
@@ -6,7 +6,6 @@
 <li id="premier"><a href="index.php?page=accueil"> Accueil </a></li>
 <li id="deuxieme"><a href="index.php?page=presentation"> Présentation </a></li>
 <li><a href="index.php?page=projets"> Projets </a></li>
-<li><a href="index.php?page=avis"> Avis </a></li>
 <li><a href="index.php?page=prendrerdv"> Rendez-vous </a></li> 
 <li id="dernier"><a href="index.php?page=contact"> Contact </a></li>
 </ul>

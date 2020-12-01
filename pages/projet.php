b2c8a9704a9122162e4456430e73ed4cb8fb1afa
Cegeeenre <tomthierry73@gmail.com>
2020-12-01 13:58:20 +0100

    Update projet.php

--- pages/projet.php
+++ pages/projet.php
@@ -1,19 +1,5 @@
 <?php
 
-
-/* print_formulairenouveauprojet();
- 
-if (isset($_SESSION['faute'])) {
-			echo "<ul>";
-			foreach($_SESSION["faute"] as $faute)
-				echo "<li>$faute</li>";
-			echo "</ul>";
-
-}*/
-
-
-//Partie refaite:
-//Utilisation de enctype, justifie par le fait qu'on encode l'upload
 ?>
 
 <section>

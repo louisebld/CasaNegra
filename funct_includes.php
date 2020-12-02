7e988446565ba5cdd8a0662d6c087caf718d13a0
Juliette-Neyrat <72942347+Juliette-Neyrat@users.noreply.github.com>
2020-12-02 16:21:04 +0100

    Update funct_includes.php

--- funct_includes.php
+++ funct_includes.php
@@ -8,6 +8,9 @@ include 'functions/funct_admin.php';
 include 'functions/funct_diapo.php';
 include 'functions/funct_projet.php';
 include 'functions/funct_contact.php';
+include 'functions/funct_rdv.php';
+
+
 
 
 include 'functions/funct_bddcompte.php';
@@ -17,5 +20,7 @@ include 'functions/funct_bddadmin.php';
 include 'functions/funct_bddprojet.php';
 include 'functions/funct_bddmetier.php';
 include 'functions/funct_bddcontact.php';
+include 'functions/funct_bddrdv.php';
+
 
 

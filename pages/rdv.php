a370dcce289fdadd56c4b986cc29ff659a71b301
Juliette-Neyrat <72942347+Juliette-Neyrat@users.noreply.github.com>
2020-12-02 16:20:37 +0100

    Update rdv.php

--- pages/rdv.php
+++ pages/rdv.php
@@ -1,6 +1,14 @@
 <section class="rdv">
 	<h2>Prendre rendez vous avec nous:</h2>
 
+<?php 
+if (isset($_SESSION['connected'])){
 
+				echo "<h2>Ajouter un RDV</h2>";
+				print_form_rdv($_SESSION['idcompte']);
+			
+			}
+
+?>
 
 </section>

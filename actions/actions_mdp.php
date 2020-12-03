4834b95c6a39c776efbecfedafa6616d8f50ea2e
Louise Bollard <louiisebld@gmail.com>
2020-12-03 17:39:53 +0100

    Update actions_mdp.php

--- actions/actions_mdp.php
+++ actions/actions_mdp.php
@@ -3,26 +3,27 @@
 if (isset($_POST['changementmdp'])) {
 
 // appel à la fonction pour savoir si le mail et le mdp correspondent
-	$verif = verificationadressemailmdp($compte, $_POST["mail"], $_POST["motdepasse"]);
+	$verif = verificationadressemailmdp($compte, $_SESSION["comptedonnee"]['mail'], $_POST["oldmdp"]);
 	$erreur=[];
 // non vide
 	
-	if (empty($_POST["motdepasse"])) {
-		$erreur[]="Vous n'avez pas renseigné le mot de passe";
+	if (empty($_POST["oldmdp"])) {
+		$erreur[]="Vous n'avez pas renseigné l'ancien mot de passe";
 
 	}
 
-	if (empty($_POST["mail"])) {
-		$erreur[]="Vous n'avez pas renseigné l'adresse mail";
+	if (empty($_POST["newmdp"])) {
+		$erreur[]="Vous n'avez pas renseigné le nouveau mot de passe";
 
 	}
+
+
 
 // Si on a des champs vide
 	if (count($erreur)>0) {
 
 // on met les valeurs dans une variable de session afin de les laisser dans le formulaire
-		$_SESSION["faute"]= $erreur;
-		$_SESSION["donneeutil"]= $_POST;
+		$_SESSION["fautechangementmdp"]= $erreur;
 
 	}
 
@@ -30,28 +31,26 @@ if (isset($_POST['changementmdp'])) {
 	elseif ($verif==false) {
 		//$_SESSION["incorrect"]=verificationadressemailmdp($compte, $_POST["mail"], $_POST["motdepasse"]);
 		// on met le tableau dans la variable de session
-		unset($_SESSION["faute"]);
+		unset($_SESSION["fautechangementmdp"]);
 		// on change la valeur de la variable de session
-		$erreur[]="Mot de passe ou adresse mail incorrect";
-		$_SESSION["faute"]=$erreur;
+		$erreur[]="Mot de passe incorrect";
+		$_SESSION["fautechangementmdp"]=$erreur;
+	}
+
+	elseif (!mdpbonformat($_POST["newmdp"])) {
+
+		$erreur[]="Votre mot de passe doit contenir au moins : • Une majuscule • Une minuscule • Un chiffre • Un caractère spécial • 8 caractères";
+
 	}
 
 // Si les deux valeurs sont correctes =2
 // on se connecte
 	else {
 // on enlève toutes les variables de session d'erreur
-		unset($_SESSION["incorrect"]);
-		unset($_SESSION["faute"]);
-		unset($_SESSION["donneeutil"]);
 
+		unset($_SESSION["fautechangementmdp"]);
 
 	//	$_SESSION['monadressemail'] = $_POST["mail"];
-
-// On prend l'id du compte
-		$_SESSION['idcompte'] = recupereid($_POST["mail"]);
-		$_SESSION['comptedonnee']=recupedonnees ($_SESSION['idcompte']);
-// on le passe en connecté
-		$_SESSION['connected']=true;
   		
 		// redirection vers une page membre
 		header('Location: index.php?page=moncompte');	

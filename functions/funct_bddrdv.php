a106cd358d338cf8e6fdbdb6b88f7a39f9fb6912
Juliette-Neyrat <72942347+Juliette-Neyrat@users.noreply.github.com>
2020-12-07 16:30:37 +0100

    Update funct_bddrdv.php

--- functions/funct_bddrdv.php
+++ functions/funct_bddrdv.php
@@ -13,10 +13,7 @@ table rdv:
   	function insert_rdv($heure, $date, $idclient, $idmembre){
 		//fonction qui ajoute un rdv dans la table rdv
 		global $c;
-		var_dump($heure);	
-		var_dump($date);
-		var_dump($idclient);
-		var_dump($idmembre);
+	
 		
 		$sql = "INSERT INTO rdv(idmembre, idclient, daterdv, heurerdv) VALUES ('$idmembre', '$idclient', '$date', '$heure')";
 
@@ -25,14 +22,14 @@ table rdv:
 
 }
 
-function charge_rdv($idrdv){
+function charge_rdv($idclient){
 	//Fonction qui renvoie calendrier avec les rdv
 	//pour le moment juste un tableau
 	
 
 	global $c;
 
-	$sql = "SELECT `idrdv`, `idmembre`, `idclient`, `daterdv`, `heurerdv` FROM `rdv` WHERE idrdv = $idrdv";
+	$sql = "SELECT idrdv, idmembre, idclient, daterdv, heurerdv FROM rdv WHERE idclient = '$idclient'";
 	$result=  mysqli_query($c, $sql);
 
 	//on met dans un tableau pour le moment 
@@ -42,6 +39,56 @@ function charge_rdv($idrdv){
 	}
 	return $tab;
 }
+
+function recupmembre($membre){
+	global $c;
+	$membres =  mysqli_query($c, "SELECT name FROM equipe ");
+	$tableau = [];
+	while ($row=mysqli_fetch_assoc($membres)) {
+		$tableau[] = $row;
+	}
+	return $tableau;
+
+}
+
+function recupmembreid($nom){
+	global $c;
+	$idmembre = mysqli_query($c, "SELECT id FROM equipe WHERE name = '$nom'");
+	$res = mysqli_fetch_assoc($idmembre);
+	return $res['id'];
+}
+
+function recupmemebrenom($id){
+	global $c;
+	$nommembre = mysqli_query($c, "SELECT name FROM equipe WHERE id = '$id'");
+	$res = mysqli_fetch_assoc($nommembre);
+	return $res['name'];
+}
+
+
+function suppr_rdv($idrdv){
+	global $c;
+	mysqli_query($c, "DELETE FROM rdv WHERE idrdv =  $idrdv");
+}
+
+
+function charge_listerdv($c) {
+
+	$req = "SELECT idclient, rdv.idmembre, daterdv, heurerdv
+	FROM rdv
+	INNER JOIN equipe ON rdv.idmembre = equipe.id";
+
+	$result=  mysqli_query($c, $req);
+
+	//on met dans un tableau
+	$tableau = [];
+	while ($row=mysqli_fetch_assoc($result)) {
+		$tableau[] = $row;
+	}
+	//var_dump($tableau);
+	return $tableau;
+}
+
 
 		?>
 

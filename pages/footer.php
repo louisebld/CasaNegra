9b057de189e7b22ef9e420c45d08aad85f739075
Juliette-Neyrat <72942347+Juliette-Neyrat@users.noreply.github.com>
2020-12-07 16:58:55 +0100

    Update footer.php

--- pages/footer.php
+++ pages/footer.php
@@ -3,7 +3,9 @@
 
 	<div id="apropos">
 		<p>
-			A PROPOS :
+			A PROPOS : <br/>
+			&copy Louise BOLLARD, Juliette NEYRAT, Tom THIERRY <br/>
+			Projet Web L2 Info
 		</p>
 	</div>
 
@@ -11,7 +13,8 @@
 
 	<div id="contact">
 		<p> CONTACT : <br/>
-			contact@casanegra.fr <br/>
+		 <a href="mailto:contact@casanegra.fr">contact@casanegra.fr</a><br/>
+
 			<a href="tel:+33626973516"> Marie au 06.26.97.35.16</a> ou <a href="tel:+33623757043"> Hervé au 06.23.76.70.43 </a></p>
 		</div>
 	</footer>

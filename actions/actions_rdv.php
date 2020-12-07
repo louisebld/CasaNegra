0eb74b302ce3d50b8cd8680843990d9a496f7dbd
Juliette-Neyrat <72942347+Juliette-Neyrat@users.noreply.github.com>
2020-12-07 16:30:32 +0100

    Update actions_rdv.php

--- actions/actions_rdv.php
+++ actions/actions_rdv.php
@@ -1,6 +1,37 @@
 <?php
 
+$listemembres = recupmembre ($c);
 // verif formulaire rdv  
 
+if (isset($_POST['equipe'])){
+	$err=[];
+
+	if (empty($_POST["heure"]) || !trim($_POST['heure'])) {
+		$erreurmetier[]="Vous n'avez pas rentré d'heure de rdv";
+		}
+
+	elseif (empty($_POST["date"]) || !trim($_POST['date'])) {
+		$erreurmetier[]="Vous n'avez pas rentré de date de rdv";
+	}
+
+	elseif (count($err)>0){
+// stockage dans une variable de session
+		$_SESSION["fauterdv"]= $err;
+
+	}
+
+	else{
+	unset($_SESSION["fauterdv"]);
+	$heure = $_POST['heure'];
+	$date = $_POST['date'];
+	$idclient = $_POST['idclient'];
+	$membre = $_POST['equipe'];
+	$idmembre = recupmembreid($membre);
+
+	insert_rdv($heure, $date, $idclient, $idmembre);
+	header('location:index.php?page=moncompte');
+
+}
+}
 
 ?>

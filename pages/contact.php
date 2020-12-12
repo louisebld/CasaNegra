c89c85ff388738431b2bfa794968412b4175c888
Louise Bollard <louiisebld@gmail.com>
2020-12-12 21:23:25 +0100

    Update contact.php

--- pages/contact.php
+++ pages/contact.php
@@ -1,27 +1,31 @@
+<!-- On importe le script de vérification du formulaire -->
 <script type='text/javascript' src='js/verifcontact.js'></script>
 
 <section class="contact">
 	<h2>Nous contacter:</h2>
 
-<?php
-if (isset($_SESSION['connected'])) {
-print_formulaire_contact();
+	<?php
+	// on affiche le formulaire seulement si on est connecté
+	if (isset($_SESSION['connected'])) {
+	// appel de la fonction pour afficher le formulaire
+		print_formulaire_contact();
+	// si on a des erreurs dans la vérification on les affiche
+		if (isset($_SESSION['fautecontact'])) {
+			echo "<ul>";
+			foreach($_SESSION["fautecontact"] as $faute)
+				echo "<li>$faute</li>";
+			echo "</ul>";
 
-if (isset($_SESSION['fautecontact'])) {
-	echo "<ul>";
-	foreach($_SESSION["fautecontact"] as $faute)
-		echo "<li>$faute</li>";
-	echo "</ul>";
-
-}
-}
-else {
-
-echo '<script>alert("Vous devez être connecté pour accéder à cette page");
+		}
+	}
+	else {
+// on informe qu'il doit être connecté pour aller sur cette page
+// redirection vers connexion du coup
+		echo '<script>alert("Vous devez être connecté pour accéder à cette page");
 		window.location.href = "./index.php?page=connexion";</script>'; 
-  		exit();
-}
+		exit();
+	}
 
 
-?>
+	?>
 </section>

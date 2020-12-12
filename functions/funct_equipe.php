49e1a7b1716a2c5bb5099369e6b803b61f095d39
Louise Bollard <louiisebld@gmail.com>
2020-12-12 14:01:10 +0100

    Update funct_equipe.php

--- functions/funct_equipe.php
+++ functions/funct_equipe.php
@@ -1,17 +1,18 @@
 <?php
 
-
 function print_equipe($equipe){
-	//Print toute l'equipe contenue dans la base de donnée
+	// Affiche les membres de l'équipe avec leur informations
+	// equipe : tableau associatif qui contient chaque membre de l'équipe
 	echo '<div class="equipe">';
+	// on parcourt la totalité
 	foreach ($equipe as $key => $value) {
+		// on affiche
 		echo '<div class="membre_equipe">';
 		echo "<h2>" . $value["name"];
 		echo " " . $value["fname"] . "</h2>";
 		echo "<p>" . $value["age"] . "ans, " . $value["metier"] . "</p>" ;
 		echo "numéro de téléphone: " . $value["tel"];
 		echo "<p>" . $value["descrip"] . "</p>";
-
 		echo "</div>";
 	}
 	echo "</div>"; 
@@ -19,14 +20,15 @@ function print_equipe($equipe){
 
 
 function print_equipeadmin($equipe){
-// fonction qui affiche les membres de l'équipe
+// fonction qui affiche les membres de l'équipe dans la page admin
+// equipe : tableau associatif qui contient chaque membre de l'équipe
 // on ajoute un champ vide qui contient l'id pour pouvoir le supprimer
 echo '<div class="equipeadmin">';
+	// on parcourt
 	foreach ($equipe as $key => $value) {
 		echo '<div class="membre_equipeadmin">';
 		echo "<p>" . $value["name"];
 		echo " " . $value["fname"] . "</p>";
-
 		echo "<form method='post' action='index.php?page=pageadmin'>";
 		echo  "<input id='idasuppr' name='idasuppr' type='hidden' value= ". $value['id'] . ">" ;
 		echo "<input type='submit' name='asupprimer' id='action' value='Supprimer'/> </form>";
@@ -34,6 +36,29 @@ echo '<div class="equipeadmin">';
 		echo "</div>";
 	}
 	echo "</div>"; 
+}
+
+
+function listeoptionmembre(){
+// affiche la liste des membres de l'équipe en radio
+// variables qui contient le tableau associatif des membres de l'équipe
+	global $listemembres;
+	?>		
+	<form name="affiche_projetequipe" action="index.php?page=projets" method="POST">
+	    			<!-- <select name="form_typeProjets"> -->
+	    			<!-- <option value="allProjets">Tous</option> -->
+	 <!-- Le tous -->
+	    			<input type='radio' id='equipe' name='projetequipe' value="allProjets" checked >
+	    			<label for='fname'>Tous</label>
+	    <?php
+	// pour chaque membre on affiche son fname dans un formulaire radio
+		foreach ($listemembres as $key => $value) {
+  					echo "<input type='radio' id='equipe' name='projetequipe' value=" . "'" . $value['fname'] . "'" . ">";
+					echo "<label for='fname'>" . $value['fname'] . "</label>";
+					} ?>
+		    <input type="submit" name="Submit" value="Chercher">
+	    	</form>
+<?php	
 }
 
 

de5709fb80743113e5d535300858847d3a4c6b68
Louise Bollard <louiisebld@gmail.com>
2020-12-12 21:23:05 +0100

    Update verifconnexion.js

--- js/verifconnexion.js
+++ js/verifconnexion.js
@@ -1,18 +1,22 @@
 function rempliconn(){
+    // Savoir si les champs sont remplis
     champ = '';
- 
+    // on intialise la variable à ''
+    // on récupère avec l'id     
+    // si l'email est vide
     if(document.getElementById("emailconn").value == ""){
         champ += "\n 'l'email'";
     }
- 
+    // si le mdp est vide
     if(document.getElementById("mdpconn").value == ""){
         champ += "\n 'Le mot de passe'";
     }
- 
+    // si champ n'a pas changé : tous les champs sont remplis
     if(champ == ""){
         return true;
     }
     else{
+        // sinon on indique et on énumère les champs
         alert("Les champs suivants doivent être remplis :" + champ);
         return false;
     }

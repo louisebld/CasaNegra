a238b75b93a03f2b899138d7aee945bda57cf9d9
Cegeeenre <tomthierry73@gmail.com>
2020-12-12 14:16:07 +0100

    Update funct_diapo.php

--- functions/funct_diapo.php
+++ functions/funct_diapo.php
@@ -17,27 +17,35 @@ for ($i = 1; $i <= 11; $i++){
 }*/
 
 function affiche_diapo(){ 
+	// BUT : affiche_diapo : Affiche les diapositives de chaque image des projets
+
 	global $c;
 
 	?>
 
 	<div class="cadre-diapo">
+	<?php
+	for ($i = 1; $i <= charge_nbphotos($c); $i++){
+
+		echo'<div class="compGen" id="diapo' . $i . '"/><a href="index.php?page=projets#diapo' . $i . '.jpg"><img class="diapo_img" src="projets/diapo' . $i .'.jpg"></a></div>';
+
+	}
+
+
+	?>
+	<div class="suivant" id="leftArrow"><h1>❯</h1></div>
+	<!-- Bouton droit -->
+
+	<div class="precedent" id="rightArrow"><h1>❮</h1></div>
+	<!-- bouton gauche -->
+
+	</div>
+
+	<script type="text/javascript" src="js/diapo.js" ></script>
+	<!-- script concernant le diapo, permet de faire defiler les images -->
+
 <?php
-for ($i = 1; $i <= charge_nbphotos($c); $i++){
 
-	echo'<div class="compGen" id="diapo' . $i . '"/><a href="index.php?page=projets#diapo' . $i . '.jpg"><img class="diapo_img" src="projets/diapo' . $i .'.jpg"></a></div>';
-
-}
-
-
-?>
-<div class="suivant" id="leftArrow"><h1>❯</h1></div>
-<div class="precedent" id="rightArrow"><h1>❮</h1></div>
-
-</div>
-
-<script type="text/javascript" src="js/diapo.js" ></script>
-<?php
 }
 
 ?>

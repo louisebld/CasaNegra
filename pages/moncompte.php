e5d2a7d7c8f6f006e33756fdd8435c2c64041517
Louise Bollard <louiisebld@gmail.com>
2020-12-12 21:23:53 +0100

    Update moncompte.php

--- pages/moncompte.php
+++ pages/moncompte.php
@@ -1,18 +1,21 @@
 <h2> Mon compte </h2>
 
 <?php
-
+// Appel de la fonction pour afficher les données c-à-d : l'email le nom et le prénom
 print_donnescompte($_SESSION['comptedonnee']);
+// on affiche ces rendez-vous
 affiche_rdv(charge_rdv($_SESSION['idcompte']));
 
 
 ?>
 <h2> Changer votre mot de passe </h2>
 <?php
+// Appel de la fonction qui affiche le formulaire de changement de mot de passe
 print_form_mdp();
-
+// Si il y a des erreurs dans les vérifications
 if (isset($_SESSION['fautechangementmdp'])) {
 	echo "<ul>";
+	// on affiche chaque erreur
 	foreach($_SESSION["fautechangementmdp"] as $faute)
 		echo "<li>$faute</li>";
 	echo "</ul>";
@@ -20,5 +23,5 @@ if (isset($_SESSION['fautechangementmdp'])) {
 }
 
 ?>
-
+<!-- On informe l'utilisateur de ce que doit contenir le mot de passe -->
 Votre nouveau mot de passe doit contenir au moins : • Une majuscule • Une minuscule • Un chiffre • Un caractère spécial • 8 caractères

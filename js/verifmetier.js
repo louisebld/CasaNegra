42745a0e217a1e0947d2f6b399c0b26b554ec575
Louise Bollard <louiisebld@gmail.com>
2020-12-12 21:23:17 +0100

    Update verifmetier.js

--- js/verifmetier.js
+++ js/verifmetier.js
@@ -1,16 +1,19 @@
 function remplimetier(){
- 
+    // Savoir si les champs sont remplis
     champ = '';
+    // on intialise la variable à ''
 
+    // si le métier est vide
      if(document.getElementById("newmetier").value == ""){
         champ += "\n 'Le métier'";
     }
  
-
+    // si champ n'a pas changé : tous les champs sont remplis
     if(champ == ""){
         return true;
     }
     else{
+        // sinon on indique et on énumère les champs
         alert("Les champs suivants doivent être remplis :" + champ);
         return false;
     }

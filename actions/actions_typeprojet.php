86e7517070b93bde8ddc9ed76a0b8a9b06bcb6ea
Louise Bollard <louiisebld@gmail.com>
2020-12-12 08:50:40 +0100

    Update actions_typeprojet.php

--- actions/actions_typeprojet.php
+++ actions/actions_typeprojet.php
@@ -16,9 +16,32 @@
 
 	//----------------- Partie concerannt l'insertion d'un nouveau type de projet ----------------------------
 
-	if (isset($_POST['newType']) && !empty($_POST['newType'])){
+	if (isset($_POST['newType']) && !empty($_POST['newType']) && trim($_POST['newType'])){
+	// verif à faire : si il y est pas déjà
 		$newType = $_POST['newType'];
 		insert_typeProjet($newType);
+		header('Location: index.php?page=projets');	
+
 	}
+
+// peut-être suppression de type
+// on peut insérer un projet vide : verif
+// supprimer un projet
+// ------------------------------ AFFICHAGE PROJET EQUIPE ---------------------------------------
+
+
+
+
+	if (isset($_POST['projetequipe']) && $_POST['projetequipe'] != "allProjets"){
+		//Si on precise un type spéciale
+		$res = charge_projetPersonne($_POST['projetequipe']);
+		$titreProjets = $_POST['projetequipe'];
+
+	} elseif (isset($_POST['projetequipe']) && $_POST['projetequipe'] = "allProjets") {
+		$res = charge_projet($c);
+		$titreProjets = "toutes nos réalisations";
+	}
+
+
 
 ?>

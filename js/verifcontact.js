25f6b3a6e847e7309f04cf9032b0ed4b0f3de2eb
Louise Bollard <louiisebld@gmail.com>
2020-12-12 21:23:07 +0100

    Update verifcontact.js

--- js/verifcontact.js
+++ js/verifcontact.js
@@ -1,20 +1,23 @@
 function remplicontact(){
-
+    // Savoir si les champs sont remplis
     champ = '';
+    // on intialise la variable à ''
+    // on récupère avec l'id     
 
+    // si le téléphone est vide
     if(document.getElementById("telephone").value == ""){
         champ += "\n 'Le téléphone'";
     }
-
+    // si le message est vide
     if(document.getElementById("message").value == ""){
         champ += "\n 'Le message'";
     }
-
-
+    // si champ n'a pas changé : tous les champs sont remplis
     if(champ == ""){
         return true;
     }
     else{
+        // sinon on indique et on énumère les champs
         alert("Les champs suivants doivent être remplis :" + champ);
         return false;
     }

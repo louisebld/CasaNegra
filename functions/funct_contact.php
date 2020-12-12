5c508b2cac7d06a239132518264b0a15172c4ee8
Louise Bollard <louiisebld@gmail.com>
2020-12-12 14:01:07 +0100

    Update funct_contact.php

--- functions/funct_contact.php
+++ functions/funct_contact.php
@@ -1,13 +1,13 @@
 <?php
 
 function print_formulaire_contact() {
-	//Affiche le formulaire pour ajouter un nouveau membre
-
+	//Affiche le formulaire pour ajouter envoyer un message de contact
 	?>
 	<div id="formulairecontact">
 	<form method="post" action="index.php?page=contact"
 	onsubmit='javascript: return remplicontact();'>
-
+<!-- On appelle la fonction javascript pour vérifier si le formulaire est rempli -->
+<!-- On affiche une valeur dans le formulaire si il y a des erreurs c-à-d on garde les données -->
 		<p>
 			<input type = "text" placeholder="Votre téléphone" name ="tel" id="telephone" value="<?php if (isset($_SESSION['telmessage']['tel'])) 
 				echo $_SESSION['telmessage']['tel']; ?>"></p>
@@ -27,7 +27,8 @@ function print_formulaire_contact() {
 
 
 function print_contact_notok($contact){
-	//Print toute l'avis contenue dans la base de donnée
+	//Affiche tous les contacts qui ne sont pas traités
+	// contact contient tous les messages de contact non traité de la base de donnée
 	echo '<div class="contactadmin">';
 	echo "<table id='tableaucontact'>";
 	echo "<tr id='firstrange'>";
@@ -37,16 +38,20 @@ function print_contact_notok($contact){
 	echo "<td> Message </td>";
 	echo "<td></td>";
 	echo "</tr>";
+	// on parcourt chaque message
 	foreach ($contact as $key => $value) {
+		// on récupère l'id du compte pour afficher le nom de la personne
 		$sesdonnees = recupedonnees ($value['idcompte']);
 		echo "<tr>";
-		echo "<td>" . $value['ladate'] . "</td>";
+		// on change la date
+		echo "<td>" . changedate($value['ladate']) . "</td>";
 		echo "<td>" . $sesdonnees['prénom'] . " " . $sesdonnees['nom'] . "</td>";
 		echo "<td>" . $value['tel'] . "</td>";
 		echo "<td>" . $value['message'] . "</td>";
 		//if ($value['etat']='atraiter') {
 		echo "<td>";
 		//echo $value['etat'];
+		// formulaire pour traiter un message
 		echo "<form method='post' action='index.php?page=pageadmin'>";
 		echo  "<input id='idcontactsuppr' name='idcontactsuppr' type='hidden' value= ". $value['idcontact'] . ">" ;
 		echo "<input type='submit' name='atraiter' id='action' value='Traiter'/> </form>";
@@ -59,7 +64,7 @@ function print_contact_notok($contact){
 }
 
 function print_contact_ok($contact){
-	//Print toute l'avis contenue dans la base de donnée
+	//Affiche tous les contacts qui sont traités
 	echo '<div class="contactadmin">';
 	echo "<table id='tableaucontact'>";
 	echo "<tr id='firstrange'>";
@@ -69,14 +74,17 @@ function print_contact_ok($contact){
 	echo "<td> Message </td>";
 	echo "<td></td>";
 	echo "</tr>";
+	// on parcourt chaque message de contact
 	foreach ($contact as $key => $value) {
+		// on récupère les données du compte pour afficher son nom et son prénom
 		$sesdonnees = recupedonnees ($value['idcompte']);
 		echo "<tr>";
-		echo "<td>" . $value['ladate'] . "</td>";
+		echo "<td>" . changedate($value['ladate']) . "</td>";
 		echo "<td>" . $sesdonnees['prénom'] . " " . $sesdonnees['nom'] . "</td>";
 		echo "<td>" . $value['tel'] . "</td>";
 		echo "<td>" . $value['message'] . "</td>";
-
+		// formulaire pour supprimer
+		// champ caché : hidden
 		echo "<td>";
 		echo "<form method='post' action='index.php?page=pageadmin'>";
 		echo  "<input id='idcontactsuppr' name='idcontactsuppr' type='hidden' value= ". $value['idcontact'] . ">" ;

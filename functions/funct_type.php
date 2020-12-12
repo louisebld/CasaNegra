cb23dedffa07f9659d545c638940c80db7f854f3
Cegeeenre <tomthierry73@gmail.com>
2020-12-12 14:16:04 +0100

    Update funct_type.php

--- functions/funct_type.php
+++ functions/funct_type.php
@@ -1,8 +1,13 @@
 <?php
 	
 	function affiche_typeProjet($typeProjets){
+		// BUT : affiche_typeProjet : affiche tous les types de projets depuis notre variable en paramaetres $typeProjets
+
+		// $typeProjets : liste contenant les types des projets, récupérés depuis la bdd typeprojet
+
 		echo '<div class="typeProjets">';
 		echo '<select name="form_typeProjets">';
+		//Affichage de tous les types dans une liste déroulantes
 		foreach ($typeProjets as $key => $value) {
 			echo '<option value="' . $value['type'] .'"">' . $value['type'] . '</option>';
 		}
@@ -12,6 +17,8 @@
 	}	
 
 	function form_newTypeProjet(){
+		// BUT : form_newTypeProjet : affiche le formulaire permettant d'ajouter un nouveau projet 
+
 		?>
 		<div class="form_newTypeProjet">
 			<form name="newTypeProjet" action="index.php?page=projets" method="POST" enctype="multipart/form-data" onsubmit='javascript: return remplitypeprojet();'></br>

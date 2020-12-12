2f63d83a627ec3106652ae32efd0f501d39a3e91
Louise Bollard <louiisebld@gmail.com>
2020-12-12 14:01:30 +0100

    Update connexion.php

--- pages/connexion.php
+++ pages/connexion.php
@@ -1,10 +1,13 @@
 <script type='text/javascript' src='js/verifconnexion.js'></script>
 <?php
+	// appel de la fonction pour afficher le formulaire
 
-print_formulaire_connexion();
+	print_formulaire_connexion();
+	// si on a des erreurs dans la vérification on les affiche
 
 if (isset($_SESSION['faute'])) {
 	echo "<ul>";
+	// on affiche chaque erreur
 	foreach($_SESSION["faute"] as $faute)
 		echo "<li>$faute</li>";
 	echo "</ul>";

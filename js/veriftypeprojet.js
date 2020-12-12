aeee4950bb19759f2fc9305715e338cf0d6ed056
Louise Bollard <louiisebld@gmail.com>
2020-12-12 21:23:20 +0100

    Update veriftypeprojet.js

--- js/veriftypeprojet.js
+++ js/veriftypeprojet.js
@@ -1,14 +1,18 @@
 function remplitypeprojet(){
+    // Savoir si les champs sont remplis
     champ = '';
- 
+    // on intialise la variable à ''
+    // si le type est vide
     if(document.getElementById("newType").value == ""){
         champ += "\n 'Le type'";
     }
- 
+
+    // si champ n'a pas changé : tous les champs sont remplis
     if(champ == ""){
         return true;
     }
     else{
+        // sinon on indique et on énumère les champs
         alert("Les champs suivants doivent être remplis :" + champ);
         return false;
     }

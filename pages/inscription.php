c58a1cbb9044bdd3d60bb3861c2a21de1b9a74fd
Cegeeenre <tomthierry73@gmail.com>
2020-12-13 22:38:29 +0100

    Update inscription.php

--- pages/inscription.php
+++ pages/inscription.php
@@ -6,26 +6,11 @@
 print_formulaire_ajout();
 
 ?>
-<!-- On informe l'utilisateur de ce que doit contenir le mot de passe -->
-<p>Votre mot de passe doit contenir au moins : • Une majuscule • Une minuscule • Un chiffre • Un caractère spécial • 8 caractères</p>
-
 
 <?php
 
 
-// si on a des erreurs dans la vérification on les affiche
-if (isset($_SESSION['erreur'])) {
 
-	echo "Vous n'avez pas renseigné les champs correctement :";
-
-	echo "<ul>";
-	// on affiche chaque erreur
-	
-	foreach($_SESSION["erreur"] as $erreur)
-		echo "<li>$erreur</li>";
-	echo "</ul>";
-
-}
 
 
 ?>

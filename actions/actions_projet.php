e98e1b05f906d0eb3993b514598066400c4ef088
Cegeeenre <tomthierry73@gmail.com>
2020-12-13 22:25:50 +0100

    Update actions_projet.php

--- actions/actions_projet.php
+++ actions/actions_projet.php
@@ -5,14 +5,18 @@
 
 
 $projets = charge_projet($c);
-	// Chargement des projets depuis la bdd projet
+// Chargement des projets depuis la bdd projet
 
 $typeProjets = charge_typeProjet($c);
-	// Chargement des types des projets, depuis la bdd typeprojet
+// Chargement des types des projets, depuis la bdd typeprojet
 
+
+// Si soumission du formulaire d'insertion de projet
 if (isset($_POST['projet'])) {
+	// Tableau qui accueilera des erreurs si il y en a
 	$erreur=[];
 
+	// Détéction des erreurs.
 	if (empty($_POST['form_typeProjets'])){
 		$erreur[]= "Le type n'est pas rempli";
 	}
@@ -37,14 +41,13 @@ if (isset($_POST['projet'])) {
 	}
 
 	if (count($erreur)>0) {
-
-// on met les valeurs dans une variable de session afin de les laisser dans le formulaire
+		// Stockage de notre tableau d'erreurs dans une variable de session afin de pouvoir les afficher plus tard
 		$_SESSION["fauteprojet"]= $erreur;
 		$_SESSION["donneesprojet"]=$_POST;
 	}
 
 	else {
-
+		// Stockage des informations depuis la varibale $_POST
 		$type = $_POST['form_typeProjets'];	
 		$date_creation = $_POST['date'];
 		$autor = $_POST['equipe'];	
@@ -56,17 +59,16 @@ if (isset($_POST['projet'])) {
 		$file_tmp =$_FILES['image']['tmp_name'];
 		$file_type=$_FILES['image']['type'];
 
+		// Incrementation du nombre de photos dans notre bdd nbphotos
 		increment_nbphotos($c);
+
+		// Recuperation du nombre de photos, pour pouvoir renommer notre projet en cours d'insertion
 		$nbphotos = charge_nbphotos($c);
 
 
 		move_uploaded_file($file_tmp,"./projets/diapo" . $nbphotos . ".jpg");
         // Permet de deplacer le fichier initial dans notre dossier projets
-
-        // On recupere les information du projet
-		
-
-		
+	
 
 		// On recupere le nom du fichier qui va etre inserer.
 		$file_new_name = "diapo" . $nbphotos . ".jpg";

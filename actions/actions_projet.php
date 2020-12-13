a3fbf83505fc1c738f36e19b4ecc47f60e90836a
Louise Bollard <louiisebld@gmail.com>
2020-12-13 17:30:27 +0100

    Update actions_projet.php

--- actions/actions_projet.php
+++ actions/actions_projet.php
@@ -1,58 +1,83 @@
 
 <?php
-	
+
 	// ------------------Insertion de projet-----------------------
 
 
-	$projets = charge_projet($c);
+$projets = charge_projet($c);
 	// Chargement des projets depuis la bdd projet
 
-	$typeProjets = charge_typeProjet($c);
+$typeProjets = charge_typeProjet($c);
 	// Chargement des types des projets, depuis la bdd typeprojet
 
+if (isset($_POST['projet'])) {
+	$erreur=[];
 
-   if(isset($_FILES['image'])){
+	if (empty($_POST['form_typeProjets'])){
+		$erreur[]= "Le type n'est pas rempli";
+	}
 
+	if (empty($_POST['date']) ){
+		$erreur[]= "Le date n'est pas remplie";
+	}
+
+	if (empty($_POST['equipe']) ){
+		$erreur[]= "L'auteur n'est pas rempli";
+	}
+
+	if (empty($_POST['description']) && !trim($_POST['description'])){
+		$erreur[]= "Le description n'est pas remplie";
+	}
+
+
+	if(empty($_FILES['image']['name'])){
+		$erreur[]= "Vous n'avez pas mis de fichier";
+
+
+	}
+
+	if (count($erreur)>0) {
+
+// on met les valeurs dans une variable de session afin de les laisser dans le formulaire
+		$_SESSION["fauteprojet"]= $erreur;
+		$_SESSION["donneesprojet"]=$_POST;
+	}
+
+	else {
+
+		$type = $_POST['form_typeProjets'];	
+		$date_creation = $_POST['date'];
+		$autor = $_POST['equipe'];	
+		$autor = $_POST['equipe'];
+		$description = $_POST['description'];
 
 		$file_name = $_FILES['image']['name'];
 		$file_size =$_FILES['image']['size'];
 		$file_tmp =$_FILES['image']['tmp_name'];
 		$file_type=$_FILES['image']['type'];
+
 		increment_nbphotos($c);
 		$nbphotos = charge_nbphotos($c);
 
 
-
-
-        move_uploaded_file($file_tmp,"./projets/diapo" . $nbphotos . ".jpg");
+		move_uploaded_file($file_tmp,"./projets/diapo" . $nbphotos . ".jpg");
         // Permet de deplacer le fichier initial dans notre dossier projets
-        
+
         // On recupere les information du projet
-        if (!empty($_POST['form_typeProjets']) || (isset($_POST['form_typeProjets']))){
-			$type = $_POST['form_typeProjets'];
-        }
+		
 
-        if (!empty($_POST['date']) || (isset($_POST['date']))){
-			$date_creation = $_POST['date'];
-
-			$autor = $_POST['equipe'];
-        }
-
-        if (!empty($_POST['equipe']) || (isset($_POST['equipe']))){
-			$autor = $_POST['equipe'];
-		}
-
-		if (!empty($_POST['description']) || (isset($_POST['description']))){
-			$description = $_POST['description'];
-		}
+		
 
 		// On recupere le nom du fichier qui va etre inserer.
 		$file_new_name = "diapo" . $nbphotos . ".jpg";
 
 		// On insere le projet
 		insert_projet($autor, $description, $file_new_name, $date_creation, $type);
-        echo "Success";
-        unset($_FILES['image']);
+		unset($_FILES['image']);
+		unset($_SESSION["fauteprojet"]);
+		unset($_SESSION["donneesprojet"]);
         header("Location: index.php?page=projets");
 
-   }
+	}
+
+}

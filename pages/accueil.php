92c58e6bfbfae58892a907a1bffc3c772ef42f44
Cegeeenre <tomthierry73@gmail.com>
2020-12-13 11:00:32 +0100

    Update accueil.php

--- pages/accueil.php
+++ pages/accueil.php
@@ -9,25 +9,6 @@
 			<p>Vous pouvez retrouver notre showroom, avec plusieurs projets déjà réalisé</p>
 </section>
 
-<!-- 
-A modifier plus tard
-Affiche un commentaire random, la note moyenne, et et le nb de projets réalisés
 
-<section class="stats">
-	<div class="box_accueil">
-		<div class="content">
-			<h2>Nombre de Projets réalisés</h2>
-					<?php //recuperer le nombre de projets de la base de données?>
-		</div>
-		<div class="content">
-			<h2>Commentaire de nos clients</h2>
-					<?php //recuperer un commentaire random de notre base de donnée?>
-		</div>
-		<div class="content">
-			<h2>Qualité des services</h2>
-					<?php //recuperer la moeynne des étoiles de notre base de donnée?>
-		</div>
-	</div>
-</section> -->
 
 

f986804222ca4acf9d96f36901bddccadce82e9b
Louise Bollard <louiisebld@gmail.com>
2020-12-14 13:16:57 +0100

    Update actions_rdv.php

--- actions/actions_rdv.php
+++ actions/actions_rdv.php
@@ -6,6 +6,7 @@ $listemembres = recupmembre ($c);
 //-------------------------------------------- verif formulaire rdv  -------------------------------
 // si sousmission d'un rdv depuis la bdd rdv
 if (isset($_POST['envoyer_rdv'])){
+
 	//tableau des erreurs si il y en a 
 	$err=[];
 
@@ -20,28 +21,27 @@ if (isset($_POST['envoyer_rdv'])){
 
 		// on verifie que ce membre de l'equipe n'a pas deja un rdv a cette heure ce jour la 
 	elseif (dejardv(($_POST['date']), $_POST['heure'], recupmembreid($_POST['equipe']))) {
-					$err[]= "ce rdv n'est pas disponible";
+					$err[]= "Ce rendez-vous n'est pas disponible, choisissez d'autres horaires";
 				}
 
 		//on verifie que les dates et les horraires rentrés sont bien dans les creaneaux de travail de l'entreprise
 				//NE FONCTIONNE PAS 
 
-	elseif (rdvnonvalide(($_POST['date']), $_POST['heure'])) {
-					$err[]= "Ce rdv n'est pas dans les creneaux de travail de l'entreprise";
-				}
+	// elseif (rdvnonvalide(($_POST['date']), $_POST['heure'])) {
+	// 				$err[]= "Ce rdv n'est pas dans les creneaux de travail de l'entreprise";
+	// 			}
 
 	
 
 	if (empty($_POST['equipe']) || !trim($_POST['date'])) {
-		$err="Vous n'avez pas rentré avec qui vous voulez prendre un rdv";
+		// ajout []
+		$err[]="Vous n'avez pas rentré avec qui vous voulez prendre un rendez-vous";
 	}
 
 	//---------------------------ON VERIFIE LE NOMBRE D'ERREUR --------------------------------
 	if (count($err)>0){
 // stockage dans de notre tableau dans une variable de session pour les afficher plus tard
-		$_SESSION["faute"]= $err;
-		$_SESSION["donneesrdv"]=$_POST;
-		var_dump($err);
+		$_SESSION["fauterdv"]= $err;
 
 	}
 
@@ -55,9 +55,7 @@ if (isset($_POST['envoyer_rdv'])){
 
 	//on insere le rdv 
 	insert_rdv($heure, $date, $idclient, $idmembre);
-	unset($err);
-	unset($_SESSION["faute"]);
-	unset($_SESSION["donneesrdv"]);
+	unset($_SESSION["fauterdv"]);
 	header('location:index.php?page=moncompte');
 
 

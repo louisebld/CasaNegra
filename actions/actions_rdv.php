a58e2e2ea7d91633578e2ceb7dc080042f0efeb9
Juliette-Neyrat <72942347+Juliette-Neyrat@users.noreply.github.com>
2020-12-14 12:43:48 +0100

    Update actions_rdv.php

--- actions/actions_rdv.php
+++ actions/actions_rdv.php
@@ -4,34 +4,63 @@
 $listemembres = recupmembre ($c);
 
 //-------------------------------------------- verif formulaire rdv  -------------------------------
-
-if (isset($_POST['equipe'])){
+// si sousmission d'un rdv depuis la bdd rdv
+if (isset($_POST['envoyer_rdv'])){
+	//tableau des erreurs si il y en a 
 	$err=[];
 
+	//---------------------------DETECTION DES ERREURS --------------------------------
 	if (empty($_POST["heure"]) || !trim($_POST['heure'])) {
-		$erreurmetier[]="Vous n'avez pas rentré d'heure de rdv";
+		$err[]="Vous n'avez pas rentré d'heure de rdv";
 		}
 
-	elseif (empty($_POST["date"]) || !trim($_POST['date'])) {
-		$erreurmetier[]="Vous n'avez pas rentré de date de rdv";
+	if (empty($_POST["date"]) || !trim($_POST['date'])) {
+		$err[]="Vous n'avez pas rentré de date de rdv";
+		}
+
+		// on verifie que ce membre de l'equipe n'a pas deja un rdv a cette heure ce jour la 
+	elseif (dejardv(($_POST['date']), $_POST['heure'], recupmembreid($_POST['equipe']))) {
+					$err[]= "ce rdv n'est pas disponible";
+				}
+
+		//on verifie que les dates et les horraires rentrés sont bien dans les creaneaux de travail de l'entreprise
+				//NE FONCTIONNE PAS 
+
+	elseif (rdvnonvalide(($_POST['date']), $_POST['heure'])) {
+					$err[]= "Ce rdv n'est pas dans les creneaux de travail de l'entreprise";
+				}
+
+	
+
+	if (empty($_POST['equipe']) || !trim($_POST['date'])) {
+		$err="Vous n'avez pas rentré avec qui vous voulez prendre un rdv";
 	}
 
-	elseif (count($err)>0){
-// stockage dans une variable de session
-		$_SESSION["fauterdv"]= $err;
+	//---------------------------ON VERIFIE LE NOMBRE D'ERREUR --------------------------------
+	if (count($err)>0){
+// stockage dans de notre tableau dans une variable de session pour les afficher plus tard
+		$_SESSION["faute"]= $err;
+		$_SESSION["donneesrdv"]=$_POST;
+		var_dump($err);
 
 	}
 
 	else{
-	unset($_SESSION["fauterdv"]);
+	//on stocke les infos depuis la variabke $_POST
 	$heure = $_POST['heure'];
 	$date = $_POST['date'];
 	$idclient = $_POST['idclient'];
 	$membre = $_POST['equipe'];
 	$idmembre = recupmembreid($membre);
 
+	//on insere le rdv 
 	insert_rdv($heure, $date, $idclient, $idmembre);
+	unset($err);
+	unset($_SESSION["faute"]);
+	unset($_SESSION["donneesrdv"]);
 	header('location:index.php?page=moncompte');
+
+
 
 }
 }

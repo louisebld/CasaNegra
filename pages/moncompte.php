2bb7b87ff8d36192e4c727ce6bac93e779d8f21f
Louise Bollard <louiisebld@gmail.com>
2020-12-14 12:09:24 +0100

    Update moncompte.php

--- pages/moncompte.php
+++ pages/moncompte.php
@@ -1,13 +1,16 @@
 <h2> Mon compte </h2>
 
+<div id='affichagemoncompte'>
+<h2> Vos informations : </h2>
 <?php
 // Appel de la fonction pour afficher les données c-à-d : l'email le nom et le prénom
 print_donnescompte($_SESSION['comptedonnee']);
 // on affiche ces rendez-vous
+echo "</div>";
+echo "<div id='affichagerdv'>";
 affiche_rdv(charge_rdv($_SESSION['idcompte']));
-
-
 ?>
+</div>
 <h2> Changer votre mot de passe </h2>
 <?php
 // Appel de la fonction qui affiche le formulaire de changement de mot de passe
